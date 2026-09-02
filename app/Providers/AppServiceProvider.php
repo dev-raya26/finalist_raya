96bf7b48897f9fb419449df3a949af2c10d0dc51
Raya Mohammed Ally <rayamohammedally34@gmail.com>
2026-09-02 02:25:04 -0700

    Update AppServiceProvider.php
    hhhhh

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,21 +7,13 @@ use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        if (app()->environment('production')) {
-            URL::forceScheme('https');
-        }
+        URL::forceScheme('https');
     }
 }
