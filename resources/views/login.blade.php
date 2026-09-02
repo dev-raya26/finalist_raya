3faad05df157e3128a44af9379a37c95d3b45d0d
Raya Mohammed Ally <rayamohammedally34@gmail.com>
2026-09-02 00:29:18 -0700

    Update login.blade.php
    leo

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,8 +3,7 @@
 <head>
     <title>Login</title>
 
-    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    
     <link rel="stylesheet"
 href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
